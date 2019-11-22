735dc3b1b5fbc72097b5255f1d9c0c06ae6e37cd
Deniz-Navarro <56900707+Deniz-Navarro@users.noreply.github.com>
2019-11-22 17:04:35 -0600

    Add files via upload

--- GreenLife/source/holi.py
+++ GreenLife/source/holi.py
@@ -1,30 +1,28 @@
-import pygame,sys
+import pygame,sys 
 from random import randint
 from pygame.locals import *
-import time
-import threading
-global seconds
-seconds = 0
+
 class Character(pygame.sprite.Sprite):
     def __init__(self,lifes):
         pygame.sprite.Sprite.__init__(self)
         self.ipersonaje = pygame.image.load("../assets/Personajes/Principal/mov.png").convert_alpha()
-        self.rect= self.ipersonaje.get_rect()
+        self.rect= self.ipersonaje.get_rect() 
         self.rect.top = 720/2
         self.rect.left = 1280/2
-        self.speed = 180
+        self.speed = 150 
         self.lifes = lifes
         self.points = 0
         segundos = "0"
 
     def draw(self,surface): #Draw Character
         surface.blit(self.ipersonaje,self.rect)
-
-    def move(self,event,termino): #Player move
+    
+    def move(self,event): #Player move
         if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
+                if event.key == pygame.K_UP: 
                     self.rect.top -= self.speed
-        if event.type == pygame.KEYUP:
+
+        if event.type == pygame.KEYUP: 
                 if event.key == pygame.K_UP:
                     self.rect.top += self.speed
 
@@ -32,38 +30,38 @@ class Character(pygame.sprite.Sprite):
         lifess = str(self.lifes)
         clifes=fuente.render("Lifes: "+lifess,0,(0,0,0))
         surface.blit(clifes,(100,650))
-
+    
     def drawp(self,surface,fuente): #Draw Points
         pointss = str(self.points)
         cpoints=fuente.render("Points: "+pointss,0,(0,0,0))
         surface.blit(cpoints,(1280/2,650))
-
+    
     def win(self,surface,fuente): #Draw Win
         lectura=fuente.render("You Win",0,(0,0,0))
-        surface.blit(lectura,(1280/2-200,720/2-200))
-
-    def lose(self,surface,fuente): #Draw Lose
+        surface.blit(lectura,(1280/2-200,720/2))
+    
+    def lose(self,surface,fuente): #Draw Lose 
         lectura=fuente.render("You Lose",0,(0,0,0))
-        surface.blit(lectura,(1280/2-200,720/2-200))
+        surface.blit(lectura,(1280/2-200,720/2))
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self,speed):
         pygame.sprite.Sprite.__init__(self)
         self.iciclista = pygame.image.load("../assets/Personajes/NPC/ciclista.png").convert_alpha()
-        self.rect= self.iciclista.get_rect()
+        self.rect= self.iciclista.get_rect() 
         self.rect.top = 720/2
         self.rect.left = 1450
         self.speed = speed
-
+        
 
     def draw(self,surface): #Enemy Draw
         surface.blit(self.iciclista,self.rect)
-
+    
     def collision(self,player): #Enemy Collision
-        (xr,yr)=(player.rect.left,player.rect.top)
+        (xr,yr)=(player.rect.left,player.rect.top) 
         self.rect.left -= self.speed
-        if self.rect.colliderect(player.rect):
-            (player.rect.left,player.rect.top)=(xr,yr)
+        if self.rect.colliderect(player.rect):  
+            (player.rect.left,player.rect.top)=(xr,yr) 
             self.rect.left= 1450
             self.rect.top = 720/2
             player.lifes-=1
@@ -75,20 +73,20 @@ class Object(pygame.sprite.Sprite):
     def __init__(self,speed,imagen):
         pygame.sprite.Sprite.__init__(self)
         self.iobject = imagen
-        self.rect= self.iobject.get_rect()
+        self.rect= self.iobject.get_rect() 
         self.rect.top = randint(200,400)
         self.rect.left = randint(1280,1500)
         self.speed = speed
-
+        
 
     def draw(self,surface): #Enemy Draw
         surface.blit(self.iobject,self.rect)
-
+    
     def collision(self,player,points): #Enemy Collision
-        (xr,yr)=(player.rect.left,player.rect.top)
+        (xr,yr)=(player.rect.left,player.rect.top) 
         self.rect.left -= self.speed
-        if self.rect.colliderect(player.rect):
-            (player.rect.left,player.rect.top)=(xr,yr)
+        if self.rect.colliderect(player.rect):  
+            (player.rect.left,player.rect.top)=(xr,yr) 
             self.rect.top = randint(200,400)
             self.rect.left = randint(1280,2000)
             player.points+=points
@@ -96,84 +94,49 @@ class Object(pygame.sprite.Sprite):
             self.rect.top = randint(200,400)
             self.rect.left = randint(1280,2000)
 
-class Cursor(pygame.Rect):
-    def __init__(self):
-        pygame.Rect.__init__(self,0,0,1,1)
-    def update(self):
-        self.left,self.top=pygame.mouse.get_pos()
-
-class Button(pygame.sprite.Sprite):
-    def __init__(self,imagen1,imagen2,x,y):
-        self.imagen_normal=imagen1
-        self.imagen_seleccion=imagen2
-        self.imagen_actual=self.imagen_normal
-        self.rect=self.imagen_actual.get_rect()
-        self.rect.left,self.rect.top=(x,y)
-    def update(self,surface,cursor):
-        if cursor.colliderect(self.rect):
-            self.imagen_actual=self.imagen_seleccion
-        else: self.imagen_actual=self.imagen_normal
-
-        surface.blit(self.imagen_actual,self.rect)
-
-def Game(endgame,fondo,life,venemy,vobject1,vobject2,goal,pointsg,tlimit):
-
-    def crono():
-        if endgame == False:
-            global seconds
-            seconds +=1
-            time.sleep(1)
-            return crono()
+def Game():
     pygame.init() #Initialize pygame modules
     ventana=pygame.display.set_mode([1280,720])
     pygame.display.set_caption("GREEN LIFE")
     #Variables
+    salir = False
     termino = False
     reloj1=pygame.time.Clock()
     fuente1 = pygame.font.SysFont ("Arial",30,True,False)
     fuente2 = pygame.font.SysFont ("Arial",100,True,False)
-    texto1= goal
+    texto1=fuente1.render("Goal: get 150 points in 25 seconds",0,(0,0,0))
     rojo=(200,20,50)
     verde=(50,205,50)
+    seconds = 0
     #Images
-    if fondo == 1:
-        ifondo = pygame.image.load("../assets/Fondos/esc11.png").convert_alpha()
-
-    if fondo == 2:
-        ifondo = pygame.image.load("../assets/Fondos/esc2.png").convert_alpha()
+    ifondo = pygame.image.load("../assets/Fondos/esc1.png").convert_alpha()
     ipapel = pygame.image.load("../assets/Items/papel.png").convert_alpha()
     iperiodico = pygame.image.load("../assets/Items/periodico.png").convert_alpha()
     #Calling Classes
-    player = Character(life)
-    ciclista = Enemy(venemy)
-    objeto = Object(vobject1,ipapel)
-    objeto2 = Object(vobject2,iperiodico)
-    if endgame!=True:
-        hilo = threading.Thread(target=crono, args=())
-        hilo.start()
-    while endgame!=True: #Loop principal
+    player = Character(5)
+    ciclista = Enemy(25)
+    objeto = Object(20,ipapel)
+    objeto2 = Object(20,iperiodico)
+
+
+    while salir!=True: #Loop principal
         for event in pygame.event.get(): #Corre todos los eventos en pygame
             if event.type == pygame.QUIT:
-                endgame=True
-                global seconds
-                seconds = 0
-            player.move(event,termino)
-        if fondo==1:
-            if seconds>tlimit/3 and seconds<=tlimit/3 *2:
-                ifondo = pygame.image.load("../assets/Fondos/esc12.png").convert_alpha()
-            if seconds>tlimit/3*2 and seconds<tlimit:
-                ifondo = pygame.image.load("../assets/Fondos/esc13.png").convert_alpha()
-
+                salir=True
+                pygame.quit()
+                sys.exit()
+            player.move(event)
+           
         #Chronometer
         if termino == False:
+            seconds = pygame.time.get_ticks()/1000
             segundos= str(seconds)
-
-        reloj1.tick(15) #Frame Counter
         #End Game
         if termino == False:
             ciclista.collision(player)
             objeto.collision(player,5)
             objeto2.collision(player,10)
+        reloj1.tick(15) #Frame Counter
         #Draw on the surface
         ventana.blit(ifondo,(0,0))
         player.draw(ventana)
@@ -182,19 +145,19 @@ def Game(endgame,fondo,life,venemy,vobject1,vobject2,goal,pointsg,tlimit):
         ventana.blit(texto1,(50,20))
         chronometer=fuente1.render(segundos,0,(0,0,0))
         ventana.blit(chronometer,(1200,650)) #Draw Chronometer
-        ciclista.draw(ventana)
+        ciclista.draw(ventana) 
         objeto.draw(ventana)
         objeto2.draw(ventana)
         #Lose Condition
-        if player.lifes == 0 or seconds >= tlimit:
+        if player.lifes == 0 or seconds == 30:
             termino = True
             ventana.fill(rojo)
-            player.lose(ventana,fuente2)
+            player.lose(ventana,fuente2)  
         #Win Condition
-        if player.points >= pointsg and seconds < tlimit:
+        if player.points >= 100 and seconds < 30:
             termino = True
             ventana.fill(verde)
-            player.win(ventana,fuente2)
-        if endgame == True:
-            seconds = 0
-        pygame.display.update()
+            player.win(ventana,fuente2)  
+        pygame.display.update()  
+
+Game()
--- GreenLife/source/juego2.py
+++ GreenLife/source/juego2.py
@@ -1,158 +1,48 @@
 import pygame,sys 
 from random import randint
 from pygame.locals import *
+from juego import *
 
-class Character(pygame.sprite.Sprite):
-    def __init__(self,lifes):
-        pygame.sprite.Sprite.__init__(self)
-        self.ipersonaje = pygame.image.load("../assets/Personajes/Principal/mov.png").convert_alpha()
-        self.rect= self.ipersonaje.get_rect() 
-        self.rect.top = 720/2
-        self.rect.left = 1280/2
-        self.speed = 150 
-        self.lifes = lifes
-        self.points = 0
-        segundos = "0"
-
-    def draw(self,surface): #Draw Character
-        surface.blit(self.ipersonaje,self.rect)
-
-    def drawl(self,surface,fuente): #Draw Lifes
-        lifess = str(self.lifes)
-        clifes=fuente.render("Lifes: "+lifess,0,(0,0,0))
-        surface.blit(clifes,(100,650))
-    
-    def drawp(self,surface,fuente): #Draw Points
-        pointss = str(self.points)
-        cpoints=fuente.render("Points: "+pointss,0,(0,0,0))
-        surface.blit(cpoints,(1280/2,650))
-    
-    def win(self,surface,fuente): #Draw Win
-        lectura=fuente.render("You Win",0,(0,0,0))
-        surface.blit(lectura,(1280/2-200,720/2))
-    
-    def lose(self,surface,fuente): #Draw Lose 
-        lectura=fuente.render("You Lose",0,(0,0,0))
-        surface.blit(lectura,(1280/2-200,720/2))
-
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self,speed):
-        pygame.sprite.Sprite.__init__(self)
-        self.iciclista = pygame.image.load("../assets/Personajes/NPC/ciclista.png").convert_alpha()
-        self.rect= self.iciclista.get_rect() 
-        self.rect.top = 720/2
-        self.rect.left = 1450
-        self.speed = speed
-        
-
-    def draw(self,surface): #Enemy Draw
-        surface.blit(self.iciclista,self.rect)
-    
-    def collision(self,player): #Enemy Collision
-        (xr,yr)=(player.rect.left,player.rect.top) 
-        self.rect.left -= self.speed
-        if self.rect.colliderect(player.rect):  
-            (player.rect.left,player.rect.top)=(xr,yr) 
-            self.rect.left= 1450
-            self.rect.top = 720/2
-            player.lifes-=1
-        if self.rect.left <= -20:
-            self.rect.left= 1450
-            self.rect.top = 720/2
-
-class Object(pygame.sprite.Sprite):
-    def __init__(self,speed,imagen):
-        pygame.sprite.Sprite.__init__(self)
-        self.iobject = imagen
-        self.rect= self.iobject.get_rect() 
-        self.rect.top = randint(200,400)
-        self.rect.left = randint(1280,1500)
-        self.speed = speed
-        
-
-    def draw(self,surface): #Enemy Draw
-        surface.blit(self.iobject,self.rect)
-    
-    def collision(self,player,points): #Enemy Collision
-        (xr,yr)=(player.rect.left,player.rect.top) 
-        self.rect.left -= self.speed
-        if self.rect.colliderect(player.rect):  
-            (player.rect.left,player.rect.top)=(xr,yr) 
-            self.rect.top = randint(200,400)
-            self.rect.left = randint(1280,2000)
-            player.points+=points
-        if self.rect.left <= -20:
-            self.rect.top = randint(200,400)
-            self.rect.left = randint(1280,2000)
-
-def main():
-    pygame.init() #Initialize pygame modules
+def menu():
+    pygame.init()
     ventana=pygame.display.set_mode([1280,720])
-    pygame.display.set_caption("GREEN LIFE")
-    #Variables
-    salir = False
-    termino = False
-    reloj1=pygame.time.Clock()
+    salir=False
     fuente1 = pygame.font.SysFont ("Arial",30,True,False)
-    fuente2 = pygame.font.SysFont ("Arial",100,True,False)
-    texto1=fuente1.render("Goal: get 100 points in 30 seconds",0,(0,0,0))
-    rojo=(200,20,50)
-    verde=(50,205,50)
-    seconds = 0
-    ifondo = pygame.image.load("../assets/Fondos/esc1.png").convert_alpha()
-    ipapel = pygame.image.load("../assets/Items/papel.png").convert_alpha()
-    iperiodico = pygame.image.load("../assets/Items/periodico.png").convert_alpha()
-    #Calling Classes
-    player = Character(5)
-    ciclista = Enemy(30)
-    objeto = Object(20,ipapel)
-    objeto2 = Object(20,iperiodico)
-
-
+    black = (0,0,0)
+    reloj1=pygame.time.Clock()
+    mundo1 = pygame.image.load("../assets/Items/mundo1.png").convert_alpha()
+    #mundo11 = pygame.image.load("../assets/Items/mundo11.png").convert_alpha()
+    #mundo2 = pygame.image.load("../assets/Items/mundo2.png").convert_alpha()
+    #mundo22 = pygame.image.load("../assets/Items/mundo22.png").convert_alpha()
+    ifondo = pygame.image.load("../assets/Fondos/Menu.png").convert_alpha()
+    start1 = pygame.image.load("../assets/Items/botons1.png").convert_alpha()
+    start2 = pygame.image.load("../assets/Items/botons2.png").convert_alpha()
+    equis1 = pygame.image.load("../assets/Items/equis.png").convert_alpha()
+    equis2 = pygame.image.load("../assets/Items/equis1.png").convert_alpha()
+    start = Button(start1,start2,1280/2-100,720/2-200)
+    equis = Button(equis1,equis2,600,510)
+    cursor1 = Cursor()
     while salir!=True: #Loop principal
         for event in pygame.event.get(): #Corre todos los eventos en pygame
             if event.type == pygame.QUIT:
                 salir=True
                 pygame.quit()
                 sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP: 
-                    player.rect.top -= player.speed
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if cursor1.colliderect(start.rect):
+                    objetive =fuente1.render("Obten 100 puntos en 30 segundos",0,(0,0,0))
+                    Game(False,1,3,25,20,20,objetive,50,25)
+                if cursor1.colliderect(equis.rect):
+                    salir=True
+                    pygame.quit()
+                    sys.exit()
 
-            if event.type == pygame.KEYUP: 
-                if event.key == pygame.K_UP:
-                    player.rect.top += player.speed
-        
-        #Chronometer
-        if termino == False:
-            seconds = pygame.time.get_ticks()/1000
-            segundos= str(seconds)
-        
-        if termino == False:
-            ciclista.collision(player)
-            objeto.collision(player,5)
-            objeto2.collision(player,10)
-        reloj1.tick(15) #Frame Counter
-        #Draw on the surface
+        #reloj1.tick(15)
+        cursor1.update()
+        ventana.fill(black)
         ventana.blit(ifondo,(0,0))
-        player.draw(ventana)
-        player.drawl(ventana,fuente1)
-        player.drawp(ventana,fuente1)
-        ventana.blit(texto1,(50,20))
-        chronometer=fuente1.render(segundos,0,(0,0,0))
-        ventana.blit(chronometer,(1200,650)) #Draw Chronometer
-        ciclista.draw(ventana) 
-        objeto.draw(ventana)
-        objeto2.draw(ventana)
-        #Lose Condition
-        if player.lifes == 0 or seconds == 30:
-            termino = True
-            ventana.fill(rojo)
-            player.lose(ventana,fuente2)  
-        if player.points >= 100 and seconds < 30:
-            termino = True
-            ventana.fill(verde)
-            player.win(ventana,fuente2)  
+        start.update(ventana,cursor1)
+        equis.update(ventana,cursor1)
         pygame.display.update()  
 
-main()
+menu()
